39a70e31df849f0ab8940a5918fb40dfffbdc768
ahmedkh67 <al.monster.games@gmail.com>
2024-11-30 09:47:40 +0300

    third commit

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -6,7 +6,7 @@ const Map = () => {
     <>
     <div className="flex w-full h-96 bg-slate-50 mt-24">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d872.9352312944584!2d-73.98237752898005!3d40.75697735360877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258ff87ce067b%3A0x43a285fbcb8352f6!2zQW1lcmljYXMgVG93ZXIsIDExNTUgNnRoIEF2ZSAjMSwgTmV3IFlvcmssIE5ZIDEwMDM22Iwg2KfZhNmI2YTYp9mK2KfYqiDYp9mE2YXYqtit2K_YqQ!5e0!3m2!1sar!2ssa!4v1731944812871!5m2!1sar!2ssa"
-        width="100%" height="full" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        width="100%" height="full" loading="lazy">
 
         </iframe>
     </div>
